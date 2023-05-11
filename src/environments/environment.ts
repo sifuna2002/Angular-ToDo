cc8758c949ef81c1ca82b13f045d8029f949bd66
sifuna2002 <sifunadone4704@gmail.com>
2023-05-11 12:46:33 +0300

    updates

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebaseConfig : {
+        apiKey: "AIzaSyAPosoLHqn6xEiNKyMLprI_E7RsdMCuAZQ",
+        authDomain: "todo-3dcc9.firebaseapp.com",
+        projectId: "todo-3dcc9",
+        storageBucket: "todo-3dcc9.appspot.com",
+        messagingSenderId: "67932797923",
+        appId: "1:67932797923:web:04e1f1ffd9da588442bed6"
+      },
+};
